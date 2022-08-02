7b371f98b44ed535e929161fc2e7c2c0fe8dcb45
Guido Zlatar <guido_zlatar07@hotmail.com>
2022-08-02 18:24:24 -0300

    Pug

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.urlencoded({extended: true}));
 
 
 const arrayProductos = [{nombre: "mochila", precio: 5000, foto:"https://cdn3.iconfinder.com/data/icons/spring-2-1/30/Backpack-256.png"},
-                   {nombre:"regla", precio: 1000, foto:"https://cdn0.iconfinder.com/data/icons/mac-applications-icons-by-omer-cetin/128/ruler.png"},
-                   {nombre:"transportador", precio: 250, foto:"https://cdn1.iconfinder.com/data/icons/office-material-9/128/curve_ruler_protactor_ruler_set_square_stationary-128.png"},
-                   {nombre:"lapicera", precio: 120, foto:"https://cdn2.iconfinder.com/data/icons/scrap/Pen.png"}]
+                        {nombre:"regla", precio: 1000, foto:"https://cdn0.iconfinder.com/data/icons/mac-applications-icons-by-omer-cetin/128/ruler.png"},
+                        {nombre:"transportador", precio: 250, foto:"https://cdn1.iconfinder.com/data/icons/office-material-9/128/curve_ruler_protactor_ruler_set_square_stationary-128.png"},
+                        {nombre:"lapicera", precio: 120, foto:"https://cdn2.iconfinder.com/data/icons/scrap/Pen.png"}]
 
 
 app.get("/", (req, res) =>{
